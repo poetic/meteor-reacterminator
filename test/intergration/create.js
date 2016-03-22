101b8afe91abe045dff93b83253e6f5d81021fd2
Chun Yang <yang2007chun@gmail.com>
2016-03-22 13:01:36 -0500

    add test for create

--- test/intergration/create.js
+++ test/intergration/create.js
@@ -1,13 +1,18 @@
 /* eslint-env mocha */
+var fs = require('fs')
 var assert = require('chai').assert
-var meterminator = require('../../../lib/index.js')
+var shelljs = require('shelljs')
+var meterminator = require('../../lib/index.js')
 
 describe('create', function () {
+  this.timeout(5 * 60 * 1000)
+
   it('create a meteor app', function () {
-    // rm test/example
-    // copy the fresh meteor app to test/example
-    // cd into the app directory
-    // run ../../meterminator.js -c
-    // check files exists
+    shelljs.rm('-rf', 'test-example')
+    shelljs.cp('-R', 'fresh-meteor-app/', 'test-example')
+    shelljs.cd('test-example')
+    meterminator('create')
+
+    assert(fs.statSync('client/main.jsx').isFile())
   })
 })
