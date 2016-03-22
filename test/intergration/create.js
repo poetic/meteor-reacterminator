0fdc1aeb3d1013fa921b15509597230fb374eb3a
Chun Yang <yang2007chun@gmail.com>
2016-03-22 12:35:38 -0500

    configure travis

---
+++ test/intergration/create.js
@@ -0,0 +1,13 @@
+/* eslint-env mocha */
+var assert = require('chai').assert
+var meterminator = require('../../../lib/index.js')
+
+describe('create', function () {
+  it('create a meteor app', function () {
+    // rm test/example
+    // copy the fresh meteor app to test/example
+    // cd into the app directory
+    // run ../../meterminator.js -c
+    // check files exists
+  })
+})
