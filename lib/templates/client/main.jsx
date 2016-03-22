b1bab6d6010f8e310d920b52d99e9b4487de3f2f
Chun Yang <yang2007chun@gmail.com>
2016-03-22 12:16:21 -0500

    bugfix: let meteor run

--- lib/templates/client/main.jsx
+++ lib/templates/client/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import { App } from './components/App.jsx';
+import App from './imports/components/App.jsx';
 
 Meteor.startup(() => {
   render(App, document.getElementById('app'));
