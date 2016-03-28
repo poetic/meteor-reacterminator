d8face7e431067c31e9e7ffd603113635813b993
Chun Yang <yang2007chun@gmail.com>
2016-03-28 12:08:40 -0500

    import image, css and js files

--- test/intergration/update.js
+++ test/intergration/update.js
@@ -1,5 +1,6 @@
 /* eslint-env mocha */
 var fs = require('fs')
+var shell = require('shelljs')
 var assert = require('chai').assert
 var meterminator = require('../../lib/index.js')
 var createMeteorProject = require('../helpers/create-meteor-project.js')
@@ -9,9 +10,12 @@ describe('update', function () {
 
   it('update a meteor app', function () {
     createMeteorProject()
-    meterminator('create')
+    shell.exec('mkdir -p client/imports/components/')
     meterminator('update')
 
     assert(fs.statSync('client/imports/components/ComponentA.jsx').isFile())
+    assert(fs.statSync('client/public/images/webflow.jpg').isFile())
+    assert(fs.statSync('client/css/webflow.css').isFile())
+    assert(fs.statSync('client/js/webflow.js').isFile())
   })
 })
