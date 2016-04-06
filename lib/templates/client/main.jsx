cf372b1c08b3c2d86edb7852a3877ca2c3982aee
Chun Yang <yang2007chun@gmail.com>
2016-04-06 12:37:21 -0500

    make main.jsx work

--- lib/templates/client/main.jsx
+++ lib/templates/client/main.jsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import App from './imports/components/App.jsx';
 
 Meteor.startup(() => {
-  render(App, document.getElementById('app'));
+  render(<App/>, document.getElementById('app'));
 });
