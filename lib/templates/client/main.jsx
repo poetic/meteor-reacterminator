bd6b74fe5aeb2bb9a55b0b18083c008da66bd96e
Chun Yang <yang2007chun@gmail.com>
2016-04-06 10:07:24 -0500

    add linter

--- lib/templates/client/main.jsx
+++ lib/templates/client/main.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import App from './imports/components/App.jsx';
 
 Meteor.startup(() => {
   render(App, document.getElementById('app'));
-})
+});
