2a17069d0dbb9cfd33edeed09c751a7679492415
Chun Yang <yang2007chun@gmail.com>
2016-04-06 12:24:01 -0500

    change stanza to React

--- lib/templates/client/imports/components/App.jsx
+++ lib/templates/client/imports/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default (
   <h1 style={{color: 'black', background: 'white'}}>
-    Stanza is working!
+    React is working!
     <br/>
     (find me at ./client/imports/components/App.jsx)
   </h1>
