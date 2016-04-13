03d18c6522d1ad86847fb1db56f873169ea5af83
Matthew Hager <matthew@poeticsystems.com>
2016-04-13 09:07:45 -0500

    Fix listing issues in App.jsx. Fixes #4

--- lib/templates/client/imports/components/App.jsx
+++ lib/templates/client/imports/components/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <h1 style={{color: 'black', background: 'white'}}>
-        React is working!
-        <br/>
-        (find me at ./client/imports/components/App.jsx)
-      </h1>
-    )
-  }
+export default function App() {
+  return (
+    <h1 style={{ color: 'black', background: 'white' }}>
+      React is working!
+      <br />
+      (find me at ./client/imports/components/App.jsx)
+    </h1>
+  );
 }
