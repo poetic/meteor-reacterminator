716cce4883c4ca612361bed80a4b3628aeb260b1
Chun Yang <yang2007chun@gmail.com>
2016-06-25 12:04:34 -0500

    remove all es6 convertion stuff

--- test/tests/unit/lib/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env mocha */
-import { assert } from 'chai';
-import stanza from '../../../../src/lib/index.js';
-
-describe('index', () => {
-  it('is a function', () => {
-    assert.isFunction(stanza);
-  });
-});
