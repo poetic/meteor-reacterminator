815b1c2682099bbff8f48ed39775f286b05b4c6c
Chun Yang <yang2007chun@gmail.com>
2016-06-25 11:59:41 -0500

    fix: remove unused dependency

--- test/tests/integration/update.js
+++ test/tests/integration/update.js
@@ -1,6 +1,5 @@
 /* eslint-env mocha */
 import fs from 'fs';
-import shell from 'shelljs';
 import { assert } from 'chai';
 import stanza from '../../../src/lib/index';
 import createMeteorProject from '../helpers/create-meteor-project';
@@ -8,7 +7,6 @@ import createMeteorProject from '../helpers/create-meteor-project';
 describe('update', () => {
   it('update a meteor app', () => {
     createMeteorProject();
-    shell.exec('mkdir -p client/imports/components/');
     stanza('update');
 
     assert(fs.statSync('client/imports/components/ComponentA.jsx').isFile());
